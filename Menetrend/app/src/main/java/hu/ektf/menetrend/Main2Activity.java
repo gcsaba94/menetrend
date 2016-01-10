a7b862fd4a1b914bb17dbbf3ce3151ad29acd540
gcsaba <g.csaba9494@freemail.hu>
2016-01-10 15:40:09 +0100

    Adatbázis

--- Menetrend/app/src/main/java/hu/ektf/menetrend/Main2Activity.java
+++ Menetrend/app/src/main/java/hu/ektf/menetrend/Main2Activity.java
@@ -3,6 +3,7 @@ package hu.ektf.menetrend;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,9 @@ import android.widget.Toast;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -67,26 +71,51 @@ public class Main2Activity extends AppCompatActivity {
             startActivity(intent);
         }
         ListView ls = (ListView) findViewById(R.id.lista);
-        /*ArrayList<Jarat> list = new ArrayList<Jarat>();
+        ArrayList<Jarat> list = new ArrayList<Jarat>();
         ArrayList<String> var = new ArrayList<>();
         var.add(start);
         var.add(end);
         Calendar cal2 = (Calendar)cal.clone();
-        cal2.add(Calendar.MINUTE,30);
-        list.add(0,new Jarat(var,cal,cal2));
-        task = new readtextfile();*/
-        //Toast.makeText(Main2Activity.this,cal.toString(),Toast.LENGTH_LONG).show();
+            /*Random r = new Random();
+            for (Integer i = 0; i < 20; i++) {
+                cal = Calendar.getInstance();
+                cal.set(Calendar.HOUR,r.nextInt(5) );
+                cal.set(Calendar.MINUTE,r.nextInt(59));
+                cal2 = (Calendar) cal.clone();
+                cal2.add(Calendar.MINUTE, r.nextInt(30));
+                list.add(i, new Jarat(var, cal, cal2));
+            }*/
+        //TODO read file
+        File sdCard = Environment.getExternalStorageDirectory();
+        File file = new File(sdCard,"menetrend.mtr");
         try {
-            task = new readtextfile(start,end,dateRepresentation,id, ls ,Main2Activity.this);
-            JaratAdapter adapter = task.execute().get();
-            ls.setAdapter(adapter);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ( (line = br.readLine())!= null && !line.startsWith(start)){}
+            line = br.readLine();
+            while ( (line = br.readLine())!= null && line.startsWith(start)){
+                if(line.endsWith(id.toString())){
+                    String[] p = line.split("|");
+                    cal.set(Calendar.HOUR,Integer.parseInt(p[0].split("#")[1].split(":")[0]));
+                    cal.set(Calendar.MINUTE, Integer.parseInt(p[0].split("#")[1].split(":")[1]));
+                    Integer i;
+                    for (i=1; i< p.length && p[i].startsWith(end); i++){}
+                    if(i<p.length){
+                        cal2.set(Calendar.HOUR,Integer.parseInt(p[i].split("#")[1].split(":")[0]));
+                        cal2.set(Calendar.MINUTE, Integer.parseInt(p[i].split("#")[1].split(":")[1]));
+                        list.add(new Jarat(var, cal, cal2));
+                    }
+                }
+            }
+            br.close();
 
-        } catch (InterruptedException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        //JaratAdapter adapter = new JaratAdapter(this,list);
+        JaratAdapter adapter = new JaratAdapter(Main2Activity.this, list);
+        ls.setAdapter(adapter);
     }
 
     int getId(Date date) {
